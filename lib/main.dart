4a3fe08a5184b3bf894ad517e95acdb53b53ca7b
Ahmed Gulab Khan <32939035+ahmedgulabkhan@users.noreply.github.com>
2020-06-13 17:34:00 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return MaterialApp(
+      title: 'Pokedex',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
